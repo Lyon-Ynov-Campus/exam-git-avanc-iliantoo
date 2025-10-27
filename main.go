db23a75e4ff1968fff9f43e811895640406541b2
iliantoo <iliantouazi@gmail.com>
2025-10-27 15:23:22 +0100

    add : func tacos (to dev)

--- main.go
+++ main.go
@@ -6,19 +6,14 @@ import (
 	"net/http"
 )
 
-<<<<<<< HEAD
-=======
 func URLHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>URL visitée</h1><p>Vous avez visité : %s</p>", r.URL.Path)
 }
 
->>>>>>> 4ed5570 (Initialize feature-url)
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Site Web en Go</h1><p>Page d'accueil</p>")
 	})
-<<<<<<< HEAD
-=======
 ...
 func ColorHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")
@@ -32,8 +27,8 @@ func main() {
 }
 	
 	http.HandleFunc("/url", URLHandler)
->>>>>>> 4ed5570 (Initialize feature-url)
 
 	fmt.Println("Serveur démarré sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+func tacos(){}
